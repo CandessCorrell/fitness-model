c797322bb1f6d59e40f39fdfb862b21247b3cbd0
CIS-gjvoit <voit_garet@bah.com>
2016-04-22 08:44:45 -0400

    Express Server working with users(GET) route

---
+++ express-server/routes/helper-functions/client.js
@@ -0,0 +1,23 @@
+'use strict';
+var pg = require("pg");
+
+// Connect to Postgres Database
+var conString = "postgres://postgres:crasufrice_jfdi16@ec2-52-91-6-140.compute-1.amazonaws.com:5432/assessment";
+
+
+var client = new pg.Client(conString);
+client.connect();
+
+exports.client = client
+
+if (!String.prototype.format) {
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+    ? args[number]
+    : match
+    ;
+  });
+};
+}
