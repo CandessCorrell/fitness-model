895474ed2d1978a272f5634ea5f8513be068ca89
Chris Stopa <christopher.m.stopa@uscis.dhs.gov>
2016-11-09 13:34:38 -0500

    undo container width edit

--- React-Frontend/src/components/home.js
+++ React-Frontend/src/components/home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
     return (
       <div>
         <h2 style={styles.header}>Fitness Model Home Page</h2>
-        <div style={styles.container} width='1200px'>
+        <div style={styles.container} width='800px'>
           <div className="row" style={styles.inner}>
             <div style={styles.inner.right} className="col-md-6">
               <img src='../../assets/final-logo.png' />
