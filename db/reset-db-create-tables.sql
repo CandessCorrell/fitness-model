35e6df840155d377f864b1bdd1cc96e45422d22b
Garet Voit <voit_garet@bah.com>
2016-12-08 22:14:58 -0500

    FIxing typos and invalid foreign key mistakes.

--- db/reset-db-create-tables.sql
+++ db/reset-db-create-tables.sql
@@ -82,25 +82,26 @@ DROP TABLE IF EXISTS divisions CASCADE;
 CREATE TABLE divisions (
   division_id serial PRIMARY KEY NOT NULL,
   description varchar(75),
-  agency_id int4 REFERENCES organizations(organization_id) NOT NULL
+  agency_id int4 REFERENCES agencies(agency_id) NOT NULL
 );
 
 DROP TABLE IF EXISTS portfolios CASCADE;
 CREATE TABLE portfolios (
   portfolio_id serial PRIMARY KEY NOT NULL,
+  description varchar(75),
   division_id int4 REFERENCES divisions(division_id)
 );
 
 DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
   product_id serial PRIMARY KEY NOT NULL,
-  division_id int4 references divisions(division_id),
-  portfolio_id varchar(75)
+  description varchar(75),  
+  portfolio_id int4 REFERENCES portfolios(portfolio_id)
 );
 
 DROP TABLE IF EXISTS teams CASCADE;
 CREATE TABLE teams (
   team_id serial PRIMARY KEY NOT NULL,
   description varchar(75),
-  product_id int4 REFERENCES branches(branch_id)
+  product_id int4 REFERENCES products(product_id)
 );
