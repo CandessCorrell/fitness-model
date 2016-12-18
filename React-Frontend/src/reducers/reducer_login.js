9f5af16af24b0b6b22b929381e477e3dc5b1fc4b
gjvoit <gjvoit@yahoo.com>
2016-12-18 14:25:41 -0500

    Login with localStorage working

--- React-Frontend/src/reducers/reducer_login.js
+++ React-Frontend/src/reducers/reducer_login.js
@@ -1,7 +1,7 @@
 // import { LOGIN, LOGOUT, REGISTER } from '../actions/index';
-import { LOGIN } from '../actions/index';
+import { LOGIN, LOGOUT } from '../actions/index';
 
-const INITIAL_STATE = { user_name: null, user_id: null};
+const INITIAL_STATE = { user_name: null, user_id: null, isLoggedIn: localStorage.getItem('isLoggedIn') };
 
 const TAG = 'REDUCER LOGIN | ';
 
@@ -10,13 +10,23 @@ export default function(state = INITIAL_STATE, action) {
 		case LOGIN:
 			console.log(TAG, 'action.payload', action.payload);
 			if (action.payload.status === 200) {
-				return { ...state, team_name: action.payload.data.rows[0].team_name, user_id: action.payload.data.rows[0].user_id };
+				const { team_name, user_id } = action.payload.data.rows[0];
+				localStorage.setItem('isLoggedIn', 'true');
+				localStorage.setItem('team_name', team_name);
+				localStorage.setItem('user_id', user_id);
+				console.log(TAG, ' \n \
+					localStorage.getItem(isLoggedIn):', localStorage.getItem('isLoggedIn') + ' \n \
+					localStorage.getItem(team_name):', localStorage.getItem('team_name') + ' \n \
+					localStorage.getItem(user_id):', localStorage.getItem('user_id')
+				);
+				return { ...state, team_name: team_name, user_id: user_id, isLoggedIn: 'true' };
 			} else {
 				console.log('Error with login, invalid credentials.');
 				return { ...state, team_name: null, user_id: null };
 			}
-    // case LOGOUT:
-    //   return { ...state, user_name: null, user_id: null }
+    case LOGOUT:
+			localStorage.setItem('isLoggedIn', 'false');
+      return { ...state, user_name: null, user_id: null, isLoggedIn: 'false' }
 		// case REGISTER:
 		// 	return { ...state, user_id: action.payload.data.rows[0].user_id };
 		default:
